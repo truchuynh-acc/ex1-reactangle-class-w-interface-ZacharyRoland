b3598f1f8203e935e801332f83d5cd17f5e3de2c
ZacharyRoland <156864477+ZacharyRoland@users.noreply.github.com>
2024-01-23 19:12:34 -0600

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,1 +1,35 @@
 /**Rectangle.cpp Code*/
+#include "Rectangle.h"
+
+Rectangle::Rectangle()
+{
+	length = 1.0;
+	width = 1.0;
+}
+
+void Rectangle::set(double newLength, double newWidth)
+{
+	if (newLength > 0.0)
+	{
+		length = newLength;
+	}
+	if (newWidth > 0.0)
+	{
+		width = newWidth;
+	}
+}
+
+double Rectangle::getLength() const
+{
+	return length;
+}
+
+double Rectangle::getWidth() const
+{
+	return width;
+}
+
+double Rectangle::getArea() const
+{
+	return length*width;
+}
