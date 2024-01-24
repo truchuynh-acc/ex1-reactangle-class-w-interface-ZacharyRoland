0d0d28bdb92510cb56720c401f5322cb090d3c0c
ZacharyRoland <156864477+ZacharyRoland@users.noreply.github.com>
2024-01-23 19:11:49 -0600

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -1,1 +1,18 @@
 /**Rectangle.h Code*/
+#ifndef RECTANGLE_
+#define RECTANGLE_
+
+#include "RectangleInterface.h"
+class Rectangle : RectangleInterface
+{
+	private:
+		double length; //the length of the rectangle
+		double width;
+	public:
+		Rectangle(); //the rectangle constructor
+		void set(double newLength, double newWidth); //the setter for the rectangle's length and width
+		double getLength() const; //the getter for the rectangle length
+		double getWidth() const; //the getter for the rectangle width
+		double getArea() const; //the getter for the rectangle area
+};
+#endif
