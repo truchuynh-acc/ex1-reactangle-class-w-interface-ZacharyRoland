018688c53baf44c32d50d4f76237488bcc3b56fc
ZacharyRoland <156864477+ZacharyRoland@users.noreply.github.com>
2024-01-23 19:11:02 -0600

    Update RectangleInterface.h

--- RectangleInterface.h
+++ RectangleInterface.h
@@ -1,1 +1,41 @@
 /** RectangleInterface Code*/
+//@RectInterface.h
+#ifndef RECTANGLE_INTERFACE_
+#define RECTANGLE_INTERFACE_
+class RectangleInterface
+{
+	public:
+		/*@param: newLength is the length for the rectangle
+				  newWidth is the width for the rectangle
+		  @pre: newLength must be greater than zero
+		  		newWidth must be greater than zero
+		  @post: if newLength is greater than zero, the length
+		  		 of the rectangle will be newLength, otherwise
+		  		 it isn't updated
+		  		 if newWidth is greater than zero, the width
+		  		 of the rectangle will be newWidth, otherwise
+		  		 it isn't updated
+		  @return: returns true if length and width are updated,
+				   false otherwise
+		*/
+		virtual void set (double newLength, double newWidth) = 0;
+		
+		/*Gets the length of the rectangle
+		  @return: returns the length of the rectangle
+		*/
+		virtual double getLength () const = 0;
+		
+		/*Gets the width of the rectangle
+		  @return: returns the width of the rectangle
+		*/
+		virtual double getWidth() const = 0;
+		
+		/*Gets the area of the rectangle
+		  @return: returns the area of the rectangle
+		*/
+		virtual double getArea() const = 0;
+		
+		/*destroys rectangle and frees it's assigned memory*/
+		virtual ~RectangleInterface() {}
+};
+#endif
